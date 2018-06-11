37cf95d13a8a24865f33aa1da82748398bf7c997
chenyalan <1448073111@qq.com>
2018-06-11 17:45:18 +0800

    first1:基础框架1

---
+++ util-foundation/src/main/java/com/zoe/spring/Application.java
@@ -0,0 +1,14 @@
+package com.zoe.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 陈亚兰 on 2018/6/11.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ util-foundation/src/main/java/com/zoe/spring/utils/Test.java
@@ -0,0 +1,13 @@
+package com.zoe.spring.utils;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 陈亚兰 on 2018/6/11.
+ */
+@RequestMapping
+@RestController
+public class Test {
+
+}
